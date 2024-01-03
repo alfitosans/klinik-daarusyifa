dd2d54a072cb07a4ec60c7291d0a6540892da32c
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-01-03 17:06:31 +0700

    fixing pembayaran

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -10,6 +10,7 @@ import danaLogo from "./../assets/dana.svg";
 import { useContext, useEffect, useState } from "react";
 import { PaymentContext } from "../context/paymentContext";
 import { Outlet, useNavigate, useParams } from "react-router";
+import { Input } from "@mui/material";
 
 const PaymentPage = () => {
   const [radioVal, setRadioVal] = useState(false);
@@ -84,10 +85,6 @@ const PaymentPage = () => {
                           Dokter {doctor.kategori} <br />
                           <br /> {doctor.instansi}
                         </p>
-                        <h5 className=" doctor-name">
-                          {" "}
-                          <span className="text-carevul ">Rp. 20000</span>
-                        </h5>
                       </Col>
                       <Col className="d-flex justify-content-center align-items-center">
                         {/* <Link to={"/list-doctor/id"} className="btn text-white fw-bold color-carevul-gradient px-5 py-2 shadow-sm">Pilih Dokter</Link> */}
@@ -98,129 +95,18 @@ const PaymentPage = () => {
               </Row>
               <Row>
                 <Col className="mt-5">
-                  <h4 className="text-carevul fw-bold">
-                    Pilih Metode Pembayaran
-                  </h4>
+                  <h4 className="text-carevul fw-bold">Validasi Pembayaran</h4>
                   <form onSubmit={handleSubmit}>
-                    {/* Gopay */}
-                    <div className="form-check ms-0 py-2 px-5 border rounded mt-1 shadow-sm  border-2 border-primary border-carevul">
-                      {/* Section Input */}
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-1 d-flex justify-content-center align-items-center">
-                            <input
-                              className="form-check-input circle-radio-btn"
-                              type="radio"
-                              name="book-date"
-                              id="gopay"
-                              value={"gopay"}
-                              onClick={(e) => handleRadioChange(e)}
-                            />
-                          </div>
-                          <div className="col">
-                            <label className="form-check-label" htmlFor="gopay">
-                              <p className="lead mt-3">
-                                <img src={gopayLogo} width={100} />
-                              </p>
-                              {/* <h5 className="text-carevul">Rp. 20000</h5> */}
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      {/* End Section Input */}
-                    </div>
-                    {/* End Gopay */}
-                    {/* shopeepay */}
-                    <div className="form-check ms-0 py-2 px-5 border rounded mt-1 shadow-sm  border-2 border-primary">
-                      {/* Section Input */}
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-1 d-flex justify-content-center align-items-center">
-                            <input
-                              className="form-check-input circle-radio-btn"
-                              type="radio"
-                              name="book-date"
-                              id="shopeepay"
-                              value={"shopeepay"}
-                              onClick={(e) => handleRadioChange(e)}
-                            />
-                          </div>
-                          <div className="col">
-                            <label
-                              className="form-check-label"
-                              htmlFor="shopeepay"
-                            >
-                              <p className="lead mt-3">
-                                <img src={shopeepayLogo} width={100} />
-                              </p>
-                              {/* <h5 className="text-carevul">Rp. 20000</h5> */}
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      {/* End Section Input */}
-                    </div>
-                    {/* End shopeepay */}
-                    {/* dana */}
-                    <div className="form-check ms-0 py-2 px-5 border rounded mt-1 shadow-sm  border-2 border-primary">
-                      {/* Section Input */}
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-1 d-flex justify-content-center align-items-center">
-                            <input
-                              className="form-check-input circle-radio-btn"
-                              type="radio"
-                              name="book-date"
-                              id="dana"
-                              value={"dana"}
-                              onClick={(e) => handleRadioChange(e)}
-                            />
-                          </div>
-                          <div className="col">
-                            <label className="form-check-label" htmlFor="dana">
-                              <p className="lead mt-3">
-                                <img src={danaLogo} width={100} />
-                              </p>
-                              {/* <h5 className="text-carevul">Rp. 20000</h5> */}
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      {/* End Section Input */}
-                    </div>
-                    {/* End dana */}
-                    {/* ovo */}
-                    <div className="form-check ms-0 py-2 px-5 border rounded mt-1 shadow-sm  border-2 border-primary">
-                      {/* Section Input */}
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-1 d-flex justify-content-center align-items-center">
-                            <input
-                              className="form-check-input circle-radio-btn"
-                              type="radio"
-                              name="book-date"
-                              id="ovo"
-                              value={"ovo"}
-                              onClick={(e) => handleRadioChange(e)}
-                            />
-                          </div>
-                          <div className="col">
-                            <label className="form-check-label" htmlFor="ovo">
-                              <p className="lead mt-3">
-                                <img src={ovoLogo} width={100} />
-                              </p>
-                              {/* <h5 className="text-carevul">Rp. 20000</h5> */}
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      {/* End Section Input */}
-                    </div>
-                    {/* End ovo */}
+                    <p>Masukan Token Anda..</p>
+                    <input
+                      type="text"
+                      placeholder="Masukan Token"
+                      className="mb-3 border-carevul rounded-3 px-3 py-2 w-100"
+                    ></input>
                     <div className="col-md-12 d-flex">
                       <button
                         type="submit"
-                        className="btn color-carevul-gradient text-white mt-4 px-5 py-2 flex-fill"
+                        className="btn color-carevul-gradient text-white mt-1 px-5 py-2 flex-fill"
                         id="book-btn"
                       >
                         Buat Janji
